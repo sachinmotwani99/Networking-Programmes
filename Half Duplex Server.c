321c95cb6891d831d3977c83d1977d13052a86e2
sachinmotwani99 <63347647+sachinmotwani99@users.noreply.github.com>
2020-10-31 16:13:12 +0530

    Update Half Duplex Server.c

--- Half Duplex Server.c
+++ Half Duplex Server.c
@@ -1,36 +1,44 @@
-
-
+#include<time.h>
 #include<sys/types.h> 
-#include<sys/socket.h> 
-#include<netinet/in.h> 
-#include<arpa/inet.h> 
-#include<netdb.h> 
+#include<sys/socket.h>
+#include<unistd.h> 
 #include<stdio.h> 
-#include<unistd.h>
+#include<string.h> 
+#include<netinet/in.h> 
+#include<netdb.h>
 int main(int argc,char *argv[])
 {
-int sd,cd;
-struct sockaddr_in servaddr,cliaddr; 
-socklen_t clilen;
-char buff[100],file[10000]; 
-struct hostent *h;
-h=gethostbyname(argv[1]); 
-bzero(&servaddr,sizeof(servaddr)); 
-servaddr.sin_family=h->h_addrtype;
-memcpy((char *)&servaddr.sin_addr.s_addr,h->h_addr_list[0],h->h_length); 
-servaddr.sin_port=htons(1500);
+int sd,ad;
+char buff[1024];
+struct sockaddr_in servaddr,cliaddr;
+socklen_t clilen=sizeof(cliaddr); 
+time_t t1; 
+bzero(&servaddr,sizeof(servaddr));
+/*Socket address structure*/ 
+servaddr.sin_family=AF_INET; 
+servaddr.sin_addr.s_addr=htonl(INADDR_ANY); 
+servaddr.sin_port=htons(1507);
+/*TCP socket is created, an Internet socket address structure is filled with wildcard address & server’s well known port*/ 
+sd=socket(AF_INET,SOCK_STREAM,0);
 
-sd=socket(AF_INET,SOCK_STREAM,0); 
-cd=connect(sd,(struct sockaddr*)&servaddr,sizeof(servaddr));
+/*Bind function assigns a local protocol address to the socket*/
+bind(sd,(struct sockaddr*)&servaddr,sizeof(servaddr));
 
+/*Listen function specifies the maximum number of connections that kernel should queue for this socket*/
+listen(sd,5);
+printf("Server is running…\n");
+/*The server to return the next completed connection from the front of the completed connection Queue calls it*/
+ad=accept(sd,(struct sockaddr *)NULL,NULL); 
 while(1)
 {
-printf("%s\n","Enter the File Name :"); 
-scanf("%s",buff); send(sd,buff,strlen(buff)+1,0); 
-printf("%s\n","File Output :"); 
-recv(sd,file,sizeof(file),0); 
-printf("%s",file);
+bzero(&buff,sizeof(buff));
+
+/*Library function time returns the Coordinated Universal Time*/
+t1=time(NULL);
+
+/*Prints the converted string format*/ 
+snprintf(buff,sizeof(buff),"%24s\r\n",ctime(&t1)); 
+send(ad,buff,sizeof(buff),0);
+}return 0;
 }
-return 0;
-}
- 
+
